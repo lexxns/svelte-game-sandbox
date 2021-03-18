624c06249c9c89e42c607aa8a8a01a6599e21e22
Jordan Aspinall <jaspinall@caci.co.uk>
2021-03-18 11:23:05 +0000

    Load in maria model

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,29 +1,29 @@
 import {
-    AmbientLight, BoxGeometry, CubeTextureLoader,
-    DirectionalLight, Mesh, MeshBasicMaterial,
-    PerspectiveCamera,
+    AmbientLight, CubeTextureLoader,
+    DirectionalLight,
+    Mesh, MeshStandardMaterial,
+    PerspectiveCamera, PlaneGeometry,
+
+
     Scene, WebGLRenderer
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
-const scene = new Scene();
+const _scene = new Scene();
 
 const fov = 60;
 const aspect = 1920 / 1080;
 const near = 1.0;
 const far = 1000.0;
-const camera = new PerspectiveCamera(fov, aspect, near, far);
+const _camera = new PerspectiveCamera(fov, aspect, near, far);
 
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
+let _loader;
+let _renderer;
+let _controls
+_camera.position.z = 5;
 
-let loader;
-let renderer;
-scene.add(cube);
-camera.position.z = 5;
-
-const setupLight = () => {
+function setupLight() {
     let light = new DirectionalLight(0xFFFFFF, 1.0);
     light.position.set(20, 100, 10);
     light.target.position.set(0, 0, 0);
@@ -39,24 +39,35 @@ const setupLight = () => {
     light.shadow.camera.right = -100;
     light.shadow.camera.top = 100;
     light.shadow.camera.bottom = -100;
-    scene.add(light);
+    _scene.add(light);
 
     light = new AmbientLight(0x101010);
-    scene.add(light);
+    _scene.add(light);
 }
 
-const setupControls = () => {
+function setupControls() {
 
     const controls = new OrbitControls(
-        camera, renderer.domElement
+        _camera, _renderer.domElement
     );
     controls.target.set(0, 20, 0);
     controls.update();
 }
 
-const loadSceneAssets = () => {
-    loader = new CubeTextureLoader();
-    let texture = loader.load([
+function loadModel() {
+    const loader = new FBXLoader();
+    loader.load("./resources/models/maria.fbx", (fbx) => {
+        fbx.scale.setScalar(0.1);
+        fbx.traverse(c => {
+            c.castShadow = true;
+        });
+        _scene.add(fbx);
+    });
+}
+
+function loadSceneAssets() {
+    _loader = new CubeTextureLoader();
+    let texture = _loader.load([
         "./resources/skycube/posx.jpg",
         "./resources/skycube/negx.jpg",
         "./resources/skycube/posy.jpg",
@@ -64,28 +75,35 @@ const loadSceneAssets = () => {
         "./resources/skycube/posz.jpg",
         "./resources/skycube/negz.jpg",
     ]);
-    scene.background = texture;
+    _scene.background = texture;
+
+    let planeGeometry = new PlaneGeometry(100, 100, 10, 10);
+    let planeMaterial = new MeshStandardMaterial({ color: 0xffffff });
+    const plane = new Mesh(planeGeometry, planeMaterial);
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    _scene.add(plane);
 }
 
-const animate = () => {
+function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+    _renderer.render(_scene, _camera);
 };
 
-const resize = () => {
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+function resize() {
+    _renderer.setSize(window.innerWidth, window.innerHeight)
+    _camera.aspect = window.innerWidth / window.innerHeight;
+    _camera.updateProjectionMatrix();
 };
 
-export const createScene = (el) => {
-    renderer = new WebGLRenderer({ antialias: true, canvas: el });
+export function createScene(el) {
+    _renderer = new WebGLRenderer({ antialias: true, canvas: el });
     resize();
     setupLight();
     setupControls();
     loadSceneAssets();
+    loadModel();
     animate();
 }
 
